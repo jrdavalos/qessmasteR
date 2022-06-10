143414f7a0085a870bb57b53e34144e9149862c3
jrdavalos <davalosjulioricardo@gmail.com>
2022-06-10 18:28:28 +0200

    ajout individu supplémentaire

--- R/tab_quali_agd.r
+++ R/tab_quali_agd.r
@@ -25,6 +25,7 @@ tab_quali_agd <- function(agd, sup_dbl = TRUE, nb = 3) {# creation de tous les t
     donnees <- get(agd$call$call$X)
     quali.act <- agd$var
     quali.sup <- agd$quali.sup
+    ind.sup <- agd$call$ind.sup
   } else if (type == "CA") {
     message("Pas encore finalise pour les AFC.")
   } else if (type == "MFA") {
@@ -32,7 +33,7 @@ tab_quali_agd <- function(agd, sup_dbl = TRUE, nb = 3) {# creation de tous les t
     donnees <- get(agd$call$call$base)
     quali.act <- agd$quali.var
     quali.sup <- agd$quali.var.sup
-
+    ind.sup <- agd$call$ind.sup
     groupes <- agd$summary.quali %>%
       rename(groupe = group) %>%
       select(variable, modalite, groupe) %>%
@@ -49,106 +50,89 @@ tab_quali_agd <- function(agd, sup_dbl = TRUE, nb = 3) {# creation de tous les t
 
   } else {message("Pas objet AGD ou type non encore finalise !")}
 
-    # variable actives
-    frequences <- donnees %>% # frequences de chaque modalite
-      pivot_longer(everything(), names_to = "variable", values_to = "modalite") %>% # prend les modalite s
-      count(variable, modalite) %>% # compte
-      group_by(variable) %>% # regroupe
-      mutate(pourcentage = 100 * n / nrow(donnees)) %>% # tri a plat des modalites
-      ungroup() %>%
-      select(variable, modalite, n, pourcentage)
-
-    var_reco <- frequences %>%
-      add_count(modalite, name = "nn") %>%
-      filter(nn > 1) %>%
-      select(variable, modalite)
-
-    if (nrow(var_reco) > 0) {
-      frequences <-  frequences %>%
-        mutate(modalite = case_when(variable %in% var_reco$variable & modalite %in% var_reco$modalite ~
-                                      case_when(is.na(modalite) ~ paste0(variable, ".NA"),
-                                                TRUE ~ paste(variable, modalite, sep = "_")),
-                                     TRUE ~ modalite))
-    }
-
-    if (type == "MFA") {
-      frequences <- groupes %>% left_join(frequences, by = c("variable", "modalite"))
-    }
-
-    coordonnees <- as.data.frame(quali.act$coord) %>%  # coordonnee de chaque modalite sur chaque axe
-      rename_all(~ paste0("Dim", str_extract(.x, "\\d+"), "_coord")) %>%
-      rownames_to_column("modalite")
-
-    contributions <- as.data.frame(quali.act$contrib) %>% # contribution de chaque modalite a chaque axe
-      rename_all(~ paste0("Dim", str_extract(.x, "\\d+"), "_contrib")) %>%
-      rownames_to_column("modalite")
-
-    cos2 <- as.data.frame(quali.act$cos2) %>% # cos2 de chaque modalite sur chaque axe
-      rename_all(~ paste0("Dim", str_extract(.x, "\\d+"), "_cos2")) %>%
-      rownames_to_column("modalite")
-
-    vtest <- as.data.frame(quali.act$v.test) %>%  # test de chaque modalite sur chaque axe
-      rename_all(~ paste0("Dim", str_extract(.x, "\\d+"), "_vtest")) %>%
-      rownames_to_column("modalite")
-
-    resultats_actives <- frequences %>%  # fusion variables actives
-      right_join(coordonnees, by = "modalite") %>%
-      right_join(contributions, by = "modalite") %>%
-      right_join(cos2, by = "modalite") %>%
-      right_join(vtest, by = "modalite")
-
-    if (type == "MCA") {
-      eta2 <- as.data.frame(quali.act$eta2) %>%
-        rename_all(~ paste0("Dim", str_extract(.x, "\\d+"), "_eta2_var")) %>%
-        rownames_to_column("variable")
-
-      resultats_actives <- resultats_actives %>% left_join(eta2, by = "variable")
-    }
-
-    resultats_actives <- resultats_actives %>% mutate(type = "Active")
-
-    # variable sup
-    coordonnees_sup <-  # coordonnee de chaque modalite sur chaque axe
-      as.data.frame(quali.sup$coord) %>%
-      rename_all(~ paste0("Dim", str_extract(.x, "\\d+"), "_coord")) %>%
-      rownames_to_column("modalite")
-
-    cos2_sup <-  # cos2 de chaque modalite sur chaque axe
-      as.data.frame(quali.sup$cos2) %>%
-      rename_all(~ paste0("Dim", str_extract(.x, "\\d+"), "_cos2")) %>%
-      rownames_to_column("modalite")
-
-    vtest_sup <-  # test de chaque modalite sur chaque axe
-      as.data.frame(quali.sup$v.test) %>%
-      rename_all(~ paste0("Dim", str_extract(.x, "\\d+"), "_vtest")) %>%
-      rownames_to_column("modalite")
-
-    resultats_sup <- frequences %>% # fusion variables sup
-      right_join(coordonnees_sup, by = "modalite") %>%
-      right_join(cos2_sup, by = "modalite") %>%
-      right_join(vtest_sup, by = "modalite")
-
-    if (type == "MCA") {
-      eta2_sup <- as.data.frame(quali.sup$eta2) %>%
-        rename_all(~ paste0("Dim", str_extract(.x, "\\d+"), "_eta2_var")) %>%
-        rownames_to_column("variable")
-
-      resultats_sup <- resultats_sup %>% left_join(eta2_sup, by = "variable")
-    }
-
-    resultats_sup <- resultats_sup %>% mutate(type = "Supplementaire")
-
-    resultats_tot <- bind_rows(resultats_actives, resultats_sup) %>% # fusion des deux grands tableaux
-      mutate(across(where(is.numeric), ~ round(.x, nb))) %>%
-      relocate(type, variable, modalite, n, pourcentage) %>%
-      relocate(ends_with("_group"), .after = last_col()) %>%
-      relocate(matches("groupe")) %>%
-      filter(!is.na(variable))
-
-    if (sup_dbl) {
-      resultats_tot %>% mutate(modalite = case_when(str_detect(modalite, ".NA") ~
-                                                      str_remove(modalite, paste0(variable, ".")),
-                                                     TRUE ~ str_remove(modalite, paste0(variable, "_"))))
-    }
-    else resultats_tot
+  if (!is.null(ind.sup)) {
+    donnees <- donnees[-ind.sup,]
+  }
+  # variable actives
+  frequences <- donnees %>% # frequences de chaque modalite
+    pivot_longer(everything(), names_to = "variable", values_to = "modalite") %>% # prend les modalite s
+    count(variable, modalite) %>% # compte
+    group_by(variable) %>% # regroupe
+    mutate(pourcentage = 100 * n / nrow(donnees)) %>% # tri a plat des modalites
+    ungroup() %>%
+    select(variable, modalite, n, pourcentage)
+  var_reco <- frequences %>%
+    add_count(modalite, name = "nn") %>%
+    filter(nn > 1) %>%
+    select(variable, modalite)
+  if (nrow(var_reco) > 0) {
+    frequences <-  frequences %>%
+      mutate(modalite = case_when(variable %in% var_reco$variable & modalite %in% var_reco$modalite ~
+                                    case_when(is.na(modalite) ~ paste0(variable, ".NA"),
+                                              TRUE ~ paste(variable, modalite, sep = "_")),
+                                   TRUE ~ modalite))
+  }
+  if (type == "MFA") {
+    frequences <- groupes %>% left_join(frequences, by = c("variable", "modalite"))
+  }
+  coordonnees <- as.data.frame(quali.act$coord) %>%  # coordonnee de chaque modalite sur chaque axe
+    rename_all(~ paste0("Dim", str_extract(.x, "\\d+"), "_coord")) %>%
+    rownames_to_column("modalite")
+  contributions <- as.data.frame(quali.act$contrib) %>% # contribution de chaque modalite a chaque axe
+    rename_all(~ paste0("Dim", str_extract(.x, "\\d+"), "_contrib")) %>%
+    rownames_to_column("modalite")
+  cos2 <- as.data.frame(quali.act$cos2) %>% # cos2 de chaque modalite sur chaque axe
+    rename_all(~ paste0("Dim", str_extract(.x, "\\d+"), "_cos2")) %>%
+    rownames_to_column("modalite")
+  vtest <- as.data.frame(quali.act$v.test) %>%  # test de chaque modalite sur chaque axe
+    rename_all(~ paste0("Dim", str_extract(.x, "\\d+"), "_vtest")) %>%
+    rownames_to_column("modalite")
+  resultats_actives <- frequences %>%  # fusion variables actives
+    right_join(coordonnees, by = "modalite") %>%
+    right_join(contributions, by = "modalite") %>%
+    right_join(cos2, by = "modalite") %>%
+    right_join(vtest, by = "modalite")
+  if (type == "MCA") {
+    eta2 <- as.data.frame(quali.act$eta2) %>%
+      rename_all(~ paste0("Dim", str_extract(.x, "\\d+"), "_eta2_var")) %>%
+      rownames_to_column("variable")
+    resultats_actives <- resultats_actives %>% left_join(eta2, by = "variable")
+  }
+  resultats_actives <- resultats_actives %>% mutate(type = "Active")
+  # variable sup
+  coordonnees_sup <-  # coordonnee de chaque modalite sur chaque axe
+    as.data.frame(quali.sup$coord) %>%
+    rename_all(~ paste0("Dim", str_extract(.x, "\\d+"), "_coord")) %>%
+    rownames_to_column("modalite")
+  cos2_sup <-  # cos2 de chaque modalite sur chaque axe
+    as.data.frame(quali.sup$cos2) %>%
+    rename_all(~ paste0("Dim", str_extract(.x, "\\d+"), "_cos2")) %>%
+    rownames_to_column("modalite")
+  vtest_sup <-  # test de chaque modalite sur chaque axe
+    as.data.frame(quali.sup$v.test) %>%
+    rename_all(~ paste0("Dim", str_extract(.x, "\\d+"), "_vtest")) %>%
+    rownames_to_column("modalite")
+  resultats_sup <- frequences %>% # fusion variables sup
+    right_join(coordonnees_sup, by = "modalite") %>%
+    right_join(cos2_sup, by = "modalite") %>%
+    right_join(vtest_sup, by = "modalite")
+  if (type == "MCA") {
+    eta2_sup <- as.data.frame(quali.sup$eta2) %>%
+      rename_all(~ paste0("Dim", str_extract(.x, "\\d+"), "_eta2_var")) %>%
+      rownames_to_column("variable")
+    resultats_sup <- resultats_sup %>% left_join(eta2_sup, by = "variable")
+  }
+  resultats_sup <- resultats_sup %>% mutate(type = "Supplementaire")
+  resultats_tot <- bind_rows(resultats_actives, resultats_sup) %>% # fusion des deux grands tableaux
+    mutate(across(where(is.numeric), ~ round(.x, nb))) %>%
+    relocate(type, variable, modalite, n, pourcentage) %>%
+    relocate(ends_with("_group"), .after = last_col()) %>%
+    relocate(matches("groupe")) %>%
+    filter(!is.na(variable))
+  if (sup_dbl) {
+    resultats_tot %>% mutate(modalite = case_when(str_detect(modalite, ".NA") ~
+                                                    str_remove(modalite, paste0(variable, ".")),
+                                                   TRUE ~ str_remove(modalite, paste0(variable, "_"))))
+  }
+  else resultats_tot
 }
