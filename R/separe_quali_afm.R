29f979f37ea2fe8fc968499422ef45082d6be6e4
jrdavalos <davalosjulioricardo@gmail.com>
2022-06-15 10:47:25 +0200

    résolution pb de noms de variables ACM/AFM

--- R/separe_quali_afm.R
+++ R/separe_quali_afm.R
@@ -12,15 +12,28 @@
 #' @export
 #'
 #' @importFrom purrr map
+#' @importFrom tidyr pivot_longer
+#' @importFrom dplyr %>% mutate across count add_count filter select case_when distinct cur_column
+#' @importFrom tidyselect everything matches
 
 separe_quali_afm <- function(afm, sup_dbl = TRUE, nb = 3, act_uniq = FALSE) {
   if (class(afm)[1] != "MFA") {stop("Pas objet AFM")}
   base <- get(afm$call$call$base)
+  var_reco <- base %>% # on doit savoir quelles modalites vont apparaitre avec leur variable
+    pivot_longer(everything(), names_to = "variable", values_to = "modalite") %>% # prend les modalite s
+    count(variable, modalite) %>% # compte
+    add_count(modalite, name = "nn") %>%
+    filter(nn > 1) %>%
+    select(variable) %>%
+    distinct() %>%
+    unlist(use.names = F)
   groupes <- c(0, cumsum(afm$call$group))
 
-  extraire <- function(groupe){
-    vars_index <- groupes[groupe]:groupes[groupe + 1]
-    assign("aux.base", base[, vars_index], envir = globalenv())
+  extraire <- function(groupe) {
+    vars_index <- (1 + groupes[groupe]):groupes[groupe + 1]
+    assign("aux.base", base[, vars_index] %>% # on recode pour avoir toutes les modalites comme il faut
+             mutate(across(matches(var_reco), ~ case_when(!is.na(.x) ~ paste(cur_column(), .x, sep = "_"),
+                                                          TRUE ~ paste0(cur_column(), ".NA")))), envir = globalenv())
     return(qessmasteR::tab_quali_agd(afm$separate.analyses[[groupe]], sup_dbl = sup_dbl, nb = nb))
   }
 
@@ -29,10 +42,12 @@ separe_quali_afm <- function(afm, sup_dbl = TRUE, nb = 3, act_uniq = FALSE) {
   if (act_uniq) {
     num_groupes <- num_groupes[-afm$call$num.group.sup]
   }
-
+  if (exists("aux.base", envir = globalenv())) {assign("baseaunomimpossiblegenrevraiment123456789", aux.base, envir = globalenv())}
   res <- map(num_groupes, ~extraire(.x))
   names(res) <- names(afm$separate.analyses)[num_groupes]
   rm(aux.base, envir = globalenv())
-
+  if (exists("baseaunomimpossiblegenrevraiment123456789", envir = globalenv())) {
+    assign("aux.base", baseaunomimpossiblegenrevraiment123456789, envir = globalenv())
+    rm(baseaunomimpossiblegenrevraiment123456789, envir = globalenv())}
   return(res)
 }
