15564d2cd53df834c875f4fc6baf6762d83e01d3
jrdavalos <davalosjulioricardo@gmail.com>
2022-06-22 00:22:04 +0200

    ajout des coordonnées partielles

--- R/tab_ind_agd.r
+++ R/tab_ind_agd.r
@@ -45,15 +45,21 @@ tab_ind_agd <- function(agd, nb = 3) {# creation de tous les tableaux pour les a
       rename_all(~ paste0("Dim", str_extract(.x, "\\d+"), "_inertie")) %>%
       rownames_to_column("ID")
 
-    coord_part <- as.data.frame(AFM_volonte_sNA$ind$coord.partiel) %>%
+    coord_part <- as.data.frame(agd$ind$coord.partiel) %>%
       rename_all(~ paste0("Dim", str_extract(.x, "\\d+"), "_coord_part")) %>%
       rownames_to_column("ID") %>%
       mutate(Groupe = str_split_fixed(ID, "\\.", 2)[,2],
              ID = str_split_fixed(ID, "\\.", 2)[,1])
-    #bind les trucs en changeant les prefixes
-    coord_part <- full_join(coord_part %>% filter(Groupe == agd$call$name.group[-agd$call$num.group.sup]))
+    noms_groupes <- agd$call$name.group[-agd$call$num.group.sup]
+    coord_part <- map_dfc(1:length(noms_groupes), ~coord_part %>%
+                            filter(Groupe == noms_groupes[.x]) %>%
+                            `colnames<-`(paste(colnames(.), .x, sep = "_"))) %>%
+      rename(ID = ID_1) %>%
+      select(ID, contains(paste0("_coord_part_", 1:length(noms_groupes))))
 
-    ind_actifs <- right_join(ind_actifs, inertie, by = "ID")
+    ind_actifs <- ind_actifs %>%
+      right_join(inertie, by = "ID") %>%
+      right_join(coord_part, by = "ID")
   }
 
   ind_actifs <- ind_actifs %>% mutate(type = "Actifs")
@@ -76,12 +82,26 @@ tab_ind_agd <- function(agd, nb = 3) {# creation de tous les tableaux pour les a
       rename_all(~ paste0("Dim", str_extract(.x, "\\d+"), "_inertie")) %>%
       rownames_to_column("ID")
 
-    ind_sup <- right_join(ind_sup, inertie_sup, by = "ID")
+    coord_part_sup <- as.data.frame(agd$ind.sup$coord.partiel) %>%
+      rename_all(~ paste0("Dim", str_extract(.x, "\\d+"), "_coord_part")) %>%
+      rownames_to_column("ID") %>%
+      mutate(Groupe = str_split_fixed(ID, "\\.", 2)[,2],
+             ID = str_split_fixed(ID, "\\.", 2)[,1])
+    coord_part_sup <- map_dfc(1:length(noms_groupes), ~coord_part_sup %>%
+                                filter(Groupe == noms_groupes[.x]) %>%
+                                `colnames<-`(paste(colnames(.), .x, sep = "_"))) %>%
+      rename(ID = ID_1) %>%
+      select(ID, contains(paste0("_coord_part_", 1:length(noms_groupes))))
+
+    ind_sup <- ind_sup %>%
+      right_join(inertie_sup, by = "ID") %>%
+      right_join(coord_part_sup, by = "ID")
   }
 
   ind_sup <- ind_sup %>% mutate(type = "Supplementaire")
 
-  ind_actifs %>% bind_rows(ind_sup) %>% # fusion des deux grands tableaux
+  ind_actifs %>%
+    bind_rows(ind_sup) %>% # fusion des deux grands tableaux
     select(-ID) %>%
     mutate(across(where(is.numeric), ~ round(.x, nb))) %>%
     relocate(starts_with("Dim"), type)
