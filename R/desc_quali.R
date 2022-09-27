3c57cb5f1b533546ca7db3cf548435cadc2d3e8b
jrdavalos <davalosjulioricardo@gmail.com>
2022-09-27 14:05:22 +0200

    ajout de desc_quali sous une forme sympathique

--- R/desc_quali.R
+++ R/desc_quali.R
@@ -1,26 +1,93 @@
-#' @title  desc_quali (beta)
-#' @author Kanto Fiaferana
+#' @title  desc_quali
+#' @author Kanto Fiaferana & Julio Ricardo Davalos
 #'
-#' @description Permet d'obtenir une description de plusieurs variables en même temps avec la fonction freq()
+#' @description Permet d'obtenir un tri à plat de plusieurs variables en même temps
 #'
+#' @param data base de données
 #' @param ... variables souhaitées
+#' @param eff TRUE par défaut. Fait apparaître les effectifs par catégorie
+#' @param freq TRUE par défaut. idem mais avec les fréquences
+#' @param cum_freq FALSE par défaut. Fréquences cumulées (veillez bien à ranger les modalités dans l'ordre souhaité)
+#' @param NR FALSE par défaut. Les non-réponses comptent-elles parmi les catégories ?
+#' @param pond NULL par défaut. Vecteur contenant les poids
+#' @param norm_pond TRUE par défaut. Normalise les poids (poids moyen = 1)
 #'
-#' @return un tableau avec les effectifs et fréquences de chaque modalité de chaque variable
+#' @return un tableau avec les effectifs et fréquences de chaque modalité de chaque variable sélectionnée
 #'
-#' @importFrom questionr freq
-#' @importFrom dplyr bind_rows
-#' @importFrom tibble tibble rownames_to_column
+#' @importFrom dplyr bind_rows mutate %>% filter group_by count rename select
+#' @importFrom tidyselect everything
+#' @importFrom purrr map map_dfr
 #'
 #' @export
-#' @examples
-#' data <- data.frame(replicate(3,sample(0:3,1000,rep=TRUE)))
-#' desc_quali(data$X1,data$X2,data$X3)
 
-desc_quali <- function(...) {
-  args <- list(...)
-  z <- lapply(args, function(x) {
-    freq(x, total = TRUE, valid = FALSE, exclude = NA)
-  })
+desc_quali <- function(data, ..., eff = TRUE, freq = TRUE, cum_freq = FALSE, NR = FALSE, pond = NULL, norm_pond = TRUE) {
+  if (cum_freq) {# si freq cumule alors forcement non cumule
+    freq <- TRUE
+  }
+  # tidyselect des variables a utiliser :
+  data_vars <- data %>% select(...)
+  if (ncol(data_vars) == 0) {
+    warning("Pas de variable selectionnee, utilisation de toutes les variables non numeriques.")
+    data_vars <- data %>% select(everything() & !where(is.numeric))
+  }
+  # liste des noms et tri si jamais pas de nom:
+  list_vars <- map(set_names(names(data_vars)), ~ quo(!!as.name(.x)))
+  # gestion de la ponderation (si vecteur ou non + pb de longueur)
+  if (!is.null(pond) & !is.vector(pond)) {
+    pond <- data %>% select({{pond}}) %>% unlist(use.names = FALSE)
+  } else if (is.vector(pond)) {
+    if (length(data_vars[,1]) != length(pond)) {
+      stop("Le vecteur de ponderation n'est pas de la bonne longueur.")
+    }
+  } else if (is.null(pond)) {# si pas de ponderation alors pas besoin de la normaliser
+    norm_pond <- FALSE
+  }
 
-  z %>% bind_rows %>% rownames_to_column %>% tibble
+  # fonction de tri a plat par variable
+  desc <- function(var) {
+    # on selectionne la variable
+    tab <- data %>% select({{var}})
+    # si ponderation on ajoute au nouveau dataframe :
+    if (!is.null(pond)) {
+      # on ajoute le vecteur de poids
+      tab <- tab %>% mutate(pond = pond)
+    }
+
+    if (!NR) {
+      tab1 <- tab
+      # on enleve les NA
+      tab <- tab %>% filter(!is.na({{var}}))
+      # s'il n'y a que des NA alors pas de lignes
+      if (nrow(tab) == 0) {
+        warning(paste(names(tab)[1]), " ne contient que des non-reponses, elles sont gardees pour cette variable.")
+        tab <- tab1
+      }
+      # normalisation de la ponderation si besoin et si demande
+      if (norm_pond & sum(pond, na.rm = T) != nrow(tab)) {
+        tab <- tab %>% mutate(pond = pond / mean(pond, na.rm = T))
+      }
+    }
+    # si pas de ponderation on fait juste les effectifs
+    if (is.null(pond)) {
+      tab <- tab %>% count({{var}}, name = "N")
+    } else { # sinon, somme des poids
+      tab <- tab %>% count({{var}}, wt = pond, name = "N")
+
+    }
+    if (freq) {# ajout des frequences
+      tab <- tab %>% mutate(Frequence = N / sum(N))
+    }
+
+    if (cum_freq) {# ajout des frequences cumulees
+      tab <- tab %>% mutate(`Frequence cumulee` = cumsum(Frequence))
+    }
+    if (!eff) {# si pas les effectifs, on les enleve
+      tab <- tab %>% select(-N)
+    }
+    # on renomme la colonne 1 "modalites" et on la met en character
+    tab %>% rename(Modalite = 1) %>% mutate(Modalite = as.character(Modalite))
+  }
+
+  # boucle pour toute la liste :
+  map_dfr(list_vars, ~desc(!!.x), .id = "Variable")
 }
