313759d1fe455d546413451d0b9337581019c441
jrdavalos <davalosjulioricardo@gmail.com>
2022-10-17 17:29:04 +0200

    Adaptations des changements de multi_quanti à desc_quanti

--- R/desc_quanti.R
+++ R/desc_quanti.R
@@ -12,23 +12,25 @@
 #' @param ic_seuil risque de première espèce pour l'intervalle de confiance.
 #' @param nb 1 par défaut. Nombre de décimales.
 #' @param med TRUE par défaut. Médiane de l'échantillon
-#' @param quart TRUE par défaut. Quartiles Q1 et Q3 de l'échantillon.
+#' @param quant 4 par défaut. Nombre de quantiles. Si la médiane est sélectionnée, elle sera ajoutée si besoin.
 #' @param minmax TRUE par défaut. Minimum et maximum .
 #' @param eff TRUE par défaut. Effectifs.
 #' @param eff_na FALSE par défaut. Remplace les effectifs par les effectifs avec les NA. N'apparait pas si eff = FALSE
-#' @param NR TRUE par défaut. Supprime les non-réponses.
+#' @param NR FALSE par défaut. Garde les non-réponses.
+#' @param msg FALSE par défaut. Envoie un message pour chaque variable terminée : utile si bug inexpliqué.
 #'
 #' @return un data.frame avec les indicateurs sélectionnés pour les variables quantitatives choisies
 #'
 #' @importFrom dplyr bind_rows mutate %>% filter group_by count rename select pull across summarise
 #' @importFrom tidyselect everything
+#' @importFrom stringr str_remove str_replace
 #' @importFrom purrr map map_dfr map_lgl keep
-#' @importFrom gmodels ci
+#' @importFrom DescTools MeanCI
 #'
 #' @export
 
 desc_quanti <- function(data, ..., moy = TRUE, sd = TRUE, test.norm = TRUE, ic = TRUE, ic_seuil = 0.05, nb = 2, med = TRUE,
-                       quart = TRUE, minmax = TRUE, eff = TRUE, eff_na = FALSE, NR = TRUE) {
+                       quant = 4, minmax = TRUE, eff = TRUE, eff_na = FALSE, NR = FALSE, msg = FALSE) {
   if (!moy) {
     ic <- FALSE
     sd <- FALSE
@@ -60,12 +62,64 @@ desc_quanti <- function(data, ..., moy = TRUE, sd = TRUE, test.norm = TRUE, ic =
     tab <- data %>% select({{var}})
     nom <- names(tab)
 
-    ######### ELEMENT A GERER PLUS TARD ##########
-    # si ponderation on ajoute au nouveau dataframe :
-    # if (!is.null(pond)) {
-      # on ajoute le vecteur de poids
-    #   tab <- tab %>% mutate(pond = pond)
-    # }
+    # on cree une fonction donnant tous les indicateurs selectionnes
+    # listes ou on ajoute au fur et a mesure :
+    fonc <- list()
+    if (eff) {# effectifs ou non
+      fonc[[length(fonc) + 1]] <- list(N = function(x) {sum(!is.na(x))})
+    }
+    if (eff_na) {# nombre de NA de la variable quanti ou non
+      fonc[[length(fonc) + 1]] <- list(NR = function(x) {sum(is.na(x))})
+    }
+    if (minmax) {
+      fonc[[length(fonc) + 1]] <- list(Min = function(x) {min(x, na.rm = TRUE)})
+      fonc[[length(fonc) + 1]] <- list(Max = function(x) {max(x, na.rm = TRUE)})
+    }
+    if (ic) {# d'abord les intervalles de confiance car calcule deja la moyenne
+      fonc[[length(fonc) + 1]] <- list(Moyenne = function(x) {
+        MeanCI(x, conf.level = (1 - ic_seuil), na.rm = TRUE) %>%
+          as.list() %>%
+          as.data.frame() %>%
+          round(nb) %>%
+          rename(Moy = mean, `IC-` = lwr.ci, `IC+` = upr.ci)
+      }
+      )
+    } else if (moy) {# si que moyenne alors calcul avec mean
+      fonc[[length(fonc) + 1]] <- list(Moy = function(x) {mean(x, na.rm = TRUE) %>% round(nb)})
+    }
+    if (sd) {# ecart-type
+      fonc[[length(fonc) + 1]] <- list(SD = function(x) {sd(x, na.rm = TRUE) %>% round(nb)})
+    }
+    if (quant > 0) {# bornes des quantiles
+      val_quant <- seq(0, 1, 1 / quant)
+      if (med & quant %% 2) { # si mediane et qu'elle est pas contenue dans les quantiles alors on l'ajoute au milieu
+        val_quant <- c(val_quant, 0.5)
+        val_quant <- val_quant[order(val_quant)]
+      }
+      if (!med) {# on enleve la mediane
+        val_quant <- val_quant[val_quant != .5]
+      }
+      val_quant <- val_quant[-c(1, length(val_quant))]
+      med <- FALSE # pour pas la faire deux fois
+      fonc[[length(fonc) + 1]] <- list(Quantiles = function(x) {
+        quantile(x, probs = val_quant, na.rm = TRUE) %>%
+          as.list() %>%
+          as.data.frame() %>%
+          rename_with(~str_remove(.x, "X") %>% str_replace("\\.", "%") %>% str_replace("50%", "Med"))
+      }
+      )
+    } else if (med) {# si pas de quantiles mais que quand meme mediane
+      fonc[[length(fonc) + 1]] <- list(Mediane = function(x) {quantile(x, probs = 0.5, na.rm = TRUE)})
+    }
+    if (test.norm) {
+      fonc[[length(fonc) + 1]] <- list(`Shapiro-Wilk` = function(x) {
+        shap <- shapiro.test(x)$p.value
+        return(format(shap, digits = 2, scientific = ifelse(shap < 0.001, T, F)))
+        }
+      )
+    }
+    # on a une liste de liste alors qu'on veut qu'une liste
+    fonc <- unlist(fonc)
 
     if (!NR) {
       tab1 <- tab
@@ -76,36 +130,14 @@ desc_quanti <- function(data, ..., moy = TRUE, sd = TRUE, test.norm = TRUE, ic =
         warning(paste(nom, " ne contient que des non-reponses, elles sont gardees pour cette variable."), call. = FALSE)
         tab <- tab1
       }
-      ######### ELEMENT A GERER PLUS TARD ##########
-      # normalisation de la ponderation si besoin et si demande
-      # if (norm_pond & sum(pond, na.rm = T) != nrow(tab)) {
-      #   tab <- tab %>% mutate(pond = pond / mean(pond, na.rm = T))
-      # }
     }
     suppressWarnings(
-      tab <- tab %>%
-        summarise(Minimum = min({{var}}, na.rm = TRUE),
-                  Maximum = max({{var}}, na.rm = TRUE),
-                  Moyenne = round(mean({{var}}, na.rm = TRUE), nb),
-                  `IC-` = round(ci({{var}}, alpha = ic_seuil, na.rm = TRUE)[2], nb),
-                  `IC+` = round(ci({{var}}, alpha = ic_seuil, na.rm = TRUE)[3], nb),
-                  `Ecart-type` = round(sd({{var}}, na.rm = TRUE), nb),
-                  `Shapiro-Wilk` =  format(shapiro.test({{var}})$p.value, digits = 2,
-                                           scientific = ifelse(shapiro.test({{var}})$p.value < 0.001, T, F)),
-                  Q1 = quantile({{var}}, probs = 0.25, na.rm = TRUE),
-                  Mediane = quantile({{var}}, probs = 0.5, na.rm = TRUE),# idee : probs = seq(0, 1, 1/n) => summarise(across())
-                  Q3 = quantile({{var}}, probs = 0.75, na.rm = TRUE),
-                  N = sum(!is.na({{var}})),
-                  `N avec NR` = n())
+      # on applique toutes les fonctions selectionnees a la variable quanti
+      tab <- tab %>% summarise(map_dfc(fonc, ~exec(.x, {{var}})))
     )
-    if (!sd) tab <- tab %>% select(-`Ecart-type`)
-    if (!ic) tab <- tab %>% select(-c(`IC-`, `IC+`))
-    if (!moy) tab <- tab %>% select(-Moyenne)
-    if (!med) tab <- tab %>% select(-Mediane)
-    if (!quart) tab <- tab %>% select(-c(Q1, Q3))
-    if (!minmax) tab <- tab %>% select(-c(Minimum, Maximum))
-    if (!eff) tab <- tab %>% select(-c(N, `N avec NR`))
-    if (eff == TRUE & eff_na == FALSE) tab <- tab %>% select(-`N avec NR`)
+    if (msg) {
+      message(paste(nom, "------------------ OK"))
+    }
     return(tab)
     }
 
